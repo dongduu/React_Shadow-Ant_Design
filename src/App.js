45d537c15ca89b53700a6570a9144d21ab2fe79c
dongduu <4012popo@naver.com>
2022-01-11 13:54:30 +0900

    feat: practice ant design

--- src/App.js
+++ src/App.js
@@ -1,72 +1,19 @@
 import logo from "./logo.svg";
-import root from "react-shadow";
-
-const styles = `
-.App {
-  text-align: center;
-}
-
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-p {
-  color: yellow;
-}
-`;
+import "./App.css";
+import { Calendar } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
 
 function App() {
   return (
-    <root.div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      <style type="text/css">{styles}</style>
-    </root.div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <GithubOutlined />
+        </p>
+        <Calendar />
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +8,6 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <p>Hello</p>
   </React.StrictMode>,
   document.getElementById("root")
 );
